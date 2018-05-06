1db7369f4e2f073240111036c4fca00b08dbea89
Arien Shibani <aaari94@gmail.com>
2018-05-06 22:17:39 +0200

    Added sexy ass gradients. Fixed the muufuccin refresh icon. Also added INFO134, we out in this bitch. Niggaaaaaaaaaaa

--- js/studyCards.js
+++ js/studyCards.js
@@ -1,13 +1,10 @@
-//Refferanser til spørsmålskort
-var card1, card2, card3, card4, card5;
-
 //JSON database
 const database = {
   "fag" : {
     "info102" : {
       "cards" : {
         "card1" : {
-          "answer" : "Universalmengden er mengden over alle elementene i et spesifikt univers.             Komplimentet av universalmengden er den tomme mengden.",
+          "answer" : "Universalmengden er mengden over alle elementene i et spesifikt univers. Komplimentet av universalmengden er den tomme mengden.",
           "keywords" : [ "mengder" ],
           "question" : "Hva er en Universalmengde?",
           "type" : "QandA"
@@ -39,7 +36,7 @@ const database = {
         "card6" : {
           "answer" : "Alle liker Kari.",
           "keywords" : [ "logikk" ],
-          "question" : "Oversett følgende predikat til muntlig språk:<br>&forall;x liker(x,Kari)",
+          "question" : "Oversett følgende predikat til naturlig språk: &forall;x liker(x,Kari)",
           "type" : "QandA"
         },
         "card7" : {
@@ -73,37 +70,29 @@ const database = {
         "card4" : {
           "answer" : "En fullstendig lenke er en lenke til et annet nettsted, og vil dermed alltid inneholde hele domenet (inkludert protokollen) til stedet det linker til. F.eks: \"http://wikipedia.com/\". En relativ lenke derimot, peker kun mot en fil som ligger på samme nettsted som det linkes fra og vil da kun være en adresse eller path. F.eks: \"index.html\" eller \"/media/image.png\".",
           "keywords" : "HTML",
-          "question" : "Hva er forskjellen på relative og absolutte lenker i <a href> ?",
+          "question" : "Hva er forskjellen på relative og absolutte lenker i &lt;a href&gt; ?",
           "type" : "QandA"
         },
         "card5" : {
           "answer" : "<ol> står for \"ordered list\" som er et element i en ordnet liste. Det vil si at man da vil få et tall forran elementet. <ul> står for \"unordered list\" og vil fremdeles være et liste element, men istedenfor tall så vil elementet bli representert med en dot/prikk istedenfor.",
           "keywords" : "HTML",
-          "question" : "Hva er forskjellen mellom <ul> og <ol>?",
+          "question" : "Hva er forskjellen mellom &lt;ul&gt; og &lt;ol&gt;?",
           "type" : "QandA"
         },
         "card6" : {
-          "question" : {
             "answer" : "ID'er er unike. Et element kan kun ha én ID tag og enhver side kan kun ha et element med den ID taggen. Klasser derimot, er ikke unike. Du kan bruke samme klasse på flere elementer og gi et element så mange klasser du vil. ",
             "keywords" : "HTML",
             "question" : "Forklar forskjellen mellom bruk av \"id\" og \"class\" i HTML elementer.",
             "type" : "QandA"
-          }
         }
       }
     }
   }
 }
 
-//På load.
-$(document).ready(main);
-
 //Main.
-function main(){
-
-	let questionPool = generateQuestionPool();
-	generateCards();
-	console.log("INFO102: " + questionPool.length + " question cards in total.")
+function main(courseCode){
+	generateCards(courseCode);
 
 	//Jquery Hendelses håndtering:
 	$('.answer').hide();
@@ -112,20 +101,41 @@ function main(){
   });
 
 	//Refresh animasjon.
-	$( ".crossRotate" ).click(function() {
-	    if (  $( this ).css( "transform" ) == 'none' ){
-	        $(this).css("transform","rotate(360deg)");
-	    } else {
-	        $(this).css("transform","" );
-	    }
-	});
+  $(".rotate").click(function(){
+    $(this).toggleClass("down")  ;
+  })
+}
+
+//Fyller ut alle 5 Study Cards med tilfeldig valgte spørsmål.
+function generateCards(courseCode){
+  let questionpool = generateQuestionPool(courseCode)
+  let currentCards = []
+  for(let i = 0;i<5;i++){
+    let randomitem = random_item(questionpool);
+    currentCards.push(randomitem);
+    questionpool = questionpool.filter(el => el !== randomitem);
+  }
+
+  console.log(currentCards);
+
+  $("#question1").append(currentCards[0].question);
+	$("#answer1").append(currentCards[0].answer);
+	$("#question2").append(currentCards[1].question);
+	$("#answer2").append(currentCards[1].answer);
+	$("#question3").append(currentCards[2].question);
+  $("#answer3").append(currentCards[2].answer);
+	$("#question4").append(currentCards[3].question);
+	$("#answer4").append(currentCards[3].answer);
+	$("#question5").append(currentCards[4].question);
+	$("#answer5").append(currentCards[4].answer);
+
 }
 
 //Returner et Array med alle kort fra et bestemt fag.
-function generateQuestionPool(){
+function generateQuestionPool(courseCode){
 
 	var questionPool = [];
-	let course = database.fag.info102.cards;
+	let course = database.fag[courseCode].cards;
 
   for (var key in course){
 		questionPool.push(course[key]);
@@ -134,58 +144,12 @@ function generateQuestionPool(){
 	return questionPool;
 }
 
-//Fyller ut alle 5 Study Cards med tilfeldig valgte spørsmål.
-function generateCards(){
-	drawFiveQuestions(generateQuestionPool());
-
-	let shuffleCounter = 0
-
-	while (card1 === card2 || //Så lenge noen kort er like..
-    card1 === card3 ||
-    card1 === card4 ||
-    card1 === card5 ||
-		card2 === card3 ||
-		card2 === card4 ||
-		card2 === card5 ||
-		card3 === card4 ||
-		card3 === card5 ||
-		card4 === card5){
-		drawFiveQuestions(generateQuestionPool()
-  ); //..stokker vi om på fordelingen
-		 shuffleCounter++;
-	}
-
-	$("#question1").append(card1.question);
-	$("#answer1").append(card1.answer);
-	$("#question2").append(card2.question);
-	$("#answer2").append(card2.answer);
-	$("#question3").append(card3.question);
-	$("#answer3").append(card3.answer);
-	$("#question4").append(card4.question);
-	$("#answer4").append(card4.answer);
-	$("#question5").append(card5.question);
-	$("#answer5").append(card5.answer);
-
-	console.log("Cards generated after " +shuffleCounter+ " shuffles.")
-}
-
-//Deler ut tilfeldige spørsmål til alle kort.
-function drawFiveQuestions(array){
-
-	card1 = random_item(array);
-	card2 = random_item(array);
-	card3 = random_item(array);
-	card4 = random_item(array);
-	card5 = random_item(array);
-}
-
 //Deler ut nye spørsmål.
-function refreshCards(){
+function refreshCards(courseCode){
 	$('.answer').hide();
 	removeCards();
-	generateCards();
+	generateCards(courseCode);
 }
-
 
 //Returner et tilfeldig element fra en ArrayList.
 function random_item(arrayList){
